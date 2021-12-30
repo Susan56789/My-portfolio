c89f54c438781cf9e8467e9a5b5f53d089ccc440
Susan Wairimu <mwihaki2susan@gmail.com>
2021-12-30 20:02:50 +0300

    installed dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScullyLibModule } from '@scullyio/ng-lib';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ScullyLibModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
