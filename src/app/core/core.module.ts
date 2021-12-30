8080c59e241e11658b3b5071e2f2f0edc3738f05
Susan Wairimu <mwihaki2susan@gmail.com>
2021-12-30 20:15:19 +0300

    core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    NgbModule,
+    HttpClientModule
+  ]
+})
+export class CoreModule { }
