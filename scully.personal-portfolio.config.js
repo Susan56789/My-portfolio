7fdf24cedff67c65525bf9399ca84f49274dea26
Susan Wairimu <mwihaki2susan@gmail.com>
2021-12-31 03:43:10 +0300

    styling

---
+++ scully.personal-portfolio.config.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    projectRoot: "./src",
+    projectName: "personal-portfolio",
+    outDir: './dist/static',
+    routes: {
+        '/blog/:slug': {
+            type: 'contentFolder',
+            slug: {
+                folder: "./blog"
+            }
+        },
+    }
+};
